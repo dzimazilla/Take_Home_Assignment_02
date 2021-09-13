8f5540729bfc7464583eff8274786a59a4616306
dzimazilla <86620578+dzimazilla@users.noreply.github.com>
2021-09-13 17:58:34 -0400

    updated "Donate'

--- scripts/script.js
+++ scripts/script.js
@@ -2,3 +2,15 @@
 
 /* Script Date: September 10, 2021 */
 
+window.onscroll = function () { myFunction() };
+
+var navbar = document.getElementById("top");
+var sticky = navbar.offsetTop;
+
+function myFunction() {
+  if (window.pageYOffset >= sticky) {
+    navbar.classList.add("sticky")
+  } else {
+    navbar.classList.remove("sticky");
+  }
+}
