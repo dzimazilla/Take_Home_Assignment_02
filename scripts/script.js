6268d6d3d0313b91645ceb0b1acdee16d78cd994
dzimazilla <86620578+dzimazilla@users.noreply.github.com>
2021-09-14 07:10:16 -0400

    created tasks.txt

--- scripts/script.js
+++ scripts/script.js
@@ -2,26 +2,6 @@
 
 /* Script Date: September 10, 2021 */
 
-
-// sticky navbar....don't working. https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_navbar_sticky
-// https://codepen.io/geoffgraham/pen/ybVzeX
-window.onscroll = function () { myFunction() };
-
-var navbar = document.getElementById("top");
-var sticky = navbar.offsetTop;
-
-function myFunction() {
-  if (window.pageYOffset >= sticky) {
-    navbar.classList.add("sticky")
-  } else {
-    navbar.classList.remove("sticky");
-  }
-}
-
-// Mobile Navigation Menu https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
-
-// sidenav https://codepen.io/chriscoyier/pen/qyELzd
-
 // Contact us form validation
 function validate(){
   var name = document.getElementById("firstName").value;
@@ -62,10 +42,8 @@ function validate(){
   return true;
 }
 
-//Sign Up for Updates
 
-//Survey? https://codepen.io/juancjara/pen/yNQqJE
-
-// index::   https://www.immigrationadvocates.org/
+// demo
+document.getElementById("demo").innerHTML = 888;
 
 
