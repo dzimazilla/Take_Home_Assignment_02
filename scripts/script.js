6c257cd3058da38eb38d8fb73dff76f65a3a3848
dzimazilla <86620578+dzimazilla@users.noreply.github.com>
2021-09-13 22:26:08 -0400

    contact us form validation

--- scripts/script.js
+++ scripts/script.js
@@ -22,7 +22,45 @@ function myFunction() {
 
 // sidenav https://codepen.io/chriscoyier/pen/qyELzd
 
-// contact form
+// Contact us form validation
+function validate(){
+  var name = document.getElementById("firstName").value;
+  var subject = document.getElementById("subject").value;
+  var phone = document.getElementById("phoneNumber").value;
+  var email = document.getElementById("emailAddress").value;
+  var message = document.getElementById("message").value;
+  var error_message = document.getElementById("errorMessage");
+  var text;
+  phoneNumber
+  
+  if(name.length < 2){
+    text = "Please Enter valid Name";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(subject.length < 5){
+    text = "Please Enter Correct Subject";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(isNaN(phone) || phone.length != 10){
+    text = "Please Enter valid Phone Number";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(email.indexOf("@") == -1 || email.length < 6){
+    text = "Please Enter valid Email";
+    error_message.innerHTML = text;
+    return false;
+  }
+  if(message.length <= 140){
+    text = "Please Enter More Than 140 Characters";
+    error_message.innerHTML = text;
+    return false;
+  }
+  alert("Form Submitted Successfully!");
+  return true;
+}
 
 //Sign Up for Updates
 
