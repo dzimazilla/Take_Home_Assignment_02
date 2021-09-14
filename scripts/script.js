28914fa3b672abae4ef39297def6334c5fd727e9
dzimazilla <86620578+dzimazilla@users.noreply.github.com>
2021-09-13 21:43:43 -0400

    added tasks in script.js

--- scripts/script.js
+++ scripts/script.js
@@ -1,7 +1,10 @@
-/* Developed by: Team 1: Perrault Christopher, Fried Norman, White Christopher, Banshchikov Dmitri, Decelles Anthony
+/* Developed by: Team 1: Perrault Christopher, Banshchikov Dmitri
 
 /* Script Date: September 10, 2021 */
 
+
+// sticky navbar....don't working. https://www.w3schools.com/howto/tryit.asp?filename=tryhow_js_navbar_sticky
+// https://codepen.io/geoffgraham/pen/ybVzeX
 window.onscroll = function () { myFunction() };
 
 var navbar = document.getElementById("top");
@@ -14,3 +17,17 @@ function myFunction() {
     navbar.classList.remove("sticky");
   }
 }
+
+// Mobile Navigation Menu https://www.w3schools.com/howto/howto_js_mobile_navbar.asp
+
+// sidenav https://codepen.io/chriscoyier/pen/qyELzd
+
+// contact form
+
+//Sign Up for Updates
+
+//Survey? https://codepen.io/juancjara/pen/yNQqJE
+
+// index::   https://www.immigrationadvocates.org/
+
+
